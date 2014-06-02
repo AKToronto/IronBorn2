11587651706b0ed118d162d76f6fa132bf816ead
AKToronto <kovarcsik@gmail.com>
2014-06-01 21:24:32 -0400

    Derp... function removed from earlier cherry-pick fix by accident

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5158,6 +5158,11 @@ void idle_task_exit(void)
 }
 
 static void calc_load_migrate(struct rq *rq)
+{
+	long delta = calc_load_fold_active(rq);
+	if (delta)
+		atomic_long_add(delta, &calc_load_tasks);
+}
 
 /*
  * While a dead CPU has no uninterruptible tasks queued at this point,
